ae216436786821b45bdb8836c1c648faa1d2f96a
Sean Tan <sean.tan@sean.tan-GHKX6X1N9C>
2024-09-25 17:31:56 +0800

    feature: base job creation setup

--- app/customer/jobs/booking/[serviceId]/clientPage.tsx
+++ app/customer/jobs/booking/[serviceId]/clientPage.tsx
@@ -1,4 +1,252 @@
+'use client';
+
+import { Copy } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { SelectValue, SelectTrigger, SelectContent, SelectItem, Select } from '@/components/ui/select';
+import { addJob } from '@/lib/actions/jobs';
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+  price: z.number(),
+  volumeDiscountPercentage: z.number(),
+  status: z.enum(['Draft', 'Active', 'Disabled']),
+});
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function BookingClient({ service }: { service: any }) {
-  return <div>{service.name}</div>;
+  const [message, setMessage] = useState('');
+  const [errors, setErrors] = useState({});
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+      price: 0,
+      volumeDiscountPercentage: 0,
+    },
+  });
+
+  const onSubmit = async () => {
+    setMessage('');
+    setErrors({});
+    const result = await addJob(form.getValues());
+    if (result?.errors) {
+      setMessage(result.message);
+      setErrors(result.errors);
+      return;
+    } else {
+      setMessage(result.message);
+      router.refresh();
+      form.reset(form.getValues());
+      router.push('/customer/jobs');
+    }
+  };
+
+  return (
+    <div className='flex w-full flex-col'>
+      <div className='flex flex-col sm:gap-4 sm:py-4'>
+        <main className='grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3'>
+          <div className='grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2'>
+            <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4'>
+              <Card className='sm:col-span-4' x-chunk='dashboard-05-chunk-0'>
+                <CardHeader className='pb-3'>
+                  <CardTitle>{service.name}</CardTitle>
+                  <CardDescription className='max-w-lg text-balance leading-relaxed'>
+                    {service.description}
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+            <Card x-chunk='dashboard-05-chunk-3'>
+              <CardHeader className='px-7'>
+                <CardTitle>Make a Booking</CardTitle>
+                <CardDescription>Payment will be made onsite.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Form {...form}>
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      form.handleSubmit(onSubmit)();
+                    }}
+                    className='max-w-md w-full flex flex-col gap-4'>
+                    <FormField
+                      control={form.control}
+                      name='name'
+                      render={({ field }) => {
+                        return (
+                          <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                              <Input placeholder='Name' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        );
+                      }}
+                    />
+                    <FormField
+                      control={form.control}
+                      name='description'
+                      render={({ field }) => {
+                        return (
+                          <FormItem>
+                            <FormLabel>Description</FormLabel>
+                            <FormControl>
+                              <Input placeholder='Description' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        );
+                      }}
+                    />
+                    <FormField
+                      control={form.control}
+                      name='price'
+                      render={({ field }) => {
+                        return (
+                          <FormItem>
+                            <FormLabel>Price</FormLabel>
+                            <FormControl>
+                              <Input
+                                type='number'
+                                placeholder='Price'
+                                {...field}
+                                onChange={(event) => field.onChange(+event.target.value)}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        );
+                      }}
+                    />
+                    <FormField
+                      control={form.control}
+                      name='volumeDiscountPercentage'
+                      render={({ field }) => {
+                        return (
+                          <FormItem>
+                            <FormLabel>Volume Discount Percentage (%)</FormLabel>
+                            <FormControl>
+                              <Input
+                                type='number'
+                                placeholder='Volume Discount Percentage (%)'
+                                {...field}
+                                onChange={(event) => field.onChange(+event.target.value)}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        );
+                      }}
+                    />
+                    <FormField
+                      control={form.control}
+                      name='status'
+                      render={({ field }) => {
+                        return (
+                          <FormItem>
+                            <FormLabel>Service Status</FormLabel>
+                            <Select onValueChange={field.onChange}>
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder='Select a service status' />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                <SelectItem value='Draft'>Draft</SelectItem>
+                                <SelectItem value='Active'>Active</SelectItem>
+                                <SelectItem value='Disabled'>Disabled</SelectItem>
+                              </SelectContent>
+                            </Select>
+                            <FormMessage />
+                          </FormItem>
+                        );
+                      }}
+                    />
+                    <Button type='submit' className='w-full'>
+                      Create Service
+                    </Button>
+                    {message ? <h2>{message}</h2> : null}
+                    {errors ? (
+                      <div className='mb-10 text-red-500'>
+                        {Object.keys(errors).map((key) => (
+                          <p key={key}>{`${key}: ${errors[key as keyof typeof errors]}`}</p>
+                        ))}
+                      </div>
+                    ) : null}
+                  </form>
+                </Form>
+              </CardContent>
+            </Card>
+          </div>
+          <div>
+            <Card className='overflow-hidden' x-chunk='dashboard-05-chunk-4'>
+              <CardHeader className='flex flex-row items-start bg-muted/50'>
+                <div className='grid gap-0.5'>
+                  <CardTitle className='group flex items-center gap-2 text-lg'>
+                    Order
+                    <Button
+                      size='icon'
+                      variant='outline'
+                      className='h-6 w-6 opacity-0 transition-opacity group-hover:opacity-100'>
+                      <Copy className='h-3 w-3' />
+                      <span className='sr-only'>Copy Order ID</span>
+                    </Button>
+                  </CardTitle>
+                  <CardDescription>Estimated Cost</CardDescription>
+                </div>
+              </CardHeader>
+              <CardContent className='p-6 text-sm'>
+                <div className='grid gap-3'>
+                  <div className='font-semibold'>Order Details</div>
+                  <ul className='grid gap-3'>
+                    <li className='flex items-center justify-between'>
+                      <span className='text-muted-foreground'>
+                        Glimmer Lamps x <span>2</span>
+                      </span>
+                      <span>$250.00</span>
+                    </li>
+                    <li className='flex items-center justify-between'>
+                      <span className='text-muted-foreground'>
+                        Aqua Filters x <span>1</span>
+                      </span>
+                      <span>$49.00</span>
+                    </li>
+                  </ul>
+                  <Separator className='my-2' />
+                  <ul className='grid gap-3'>
+                    <li className='flex items-center justify-between'>
+                      <span className='text-muted-foreground'>Subtotal</span>
+                      <span>$299.00</span>
+                    </li>
+                    <li className='flex items-center justify-between'>
+                      <span className='text-muted-foreground'>GST</span>
+                      <span>$25.00</span>
+                    </li>
+                    <li className='flex items-center justify-between font-semibold'>
+                      <span className='text-muted-foreground'>Total</span>
+                      <span>$329.00</span>
+                    </li>
+                  </ul>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
