a6596e1827d73f614c36917b95c3cc39120610f1
Sean Tan <sean.tan@sean.tan-GHKX6X1N9C>
2024-09-25 19:52:42 +0800

    feature: add contact us

--- app/customer/jobs/booking/[serviceId]/clientPage.tsx
+++ app/customer/jobs/booking/[serviceId]/clientPage.tsx
@@ -59,8 +59,8 @@ export default function BookingClient({ service }: { service: any }) {
       <div className='flex flex-col sm:gap-4 sm:py-4'>
         <main className='grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3'>
           <div className='grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2'>
-            <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4'>
-              <Card className='sm:col-span-4' x-chunk='dashboard-05-chunk-0'>
+            <div className='grid gap-4 md:grid-cols-4'>
+              <Card className='md:col-span-2' x-chunk='dashboard-05-chunk-0'>
                 <CardHeader className='pb-3'>
                   <CardTitle>{service.name}</CardTitle>
                   <CardDescription className='max-w-lg text-balance leading-relaxed'>
@@ -68,6 +68,115 @@ export default function BookingClient({ service }: { service: any }) {
                   </CardDescription>
                 </CardHeader>
               </Card>
+              <a href='https://wa.me/6596770773' target='_blank' rel='noreferrer'>
+                <Card className='md:col-span-1' x-chunk='dashboard-05-chunk-1'>
+                  <CardHeader className='pb-2'>
+                    <CardDescription>Questions?</CardDescription>
+                    <CardTitle className='text-2xl'>96770773</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className='text-xs text-muted-foreground'>Contact Us</div>
+                  </CardContent>
+                </Card>
+              </a>
+              <a href='https://maps.app.goo.gl/n6cbgUSQMymZbnyd8' target='_blank' rel='noreferrer'>
+                <Card className='md:col-span-1' x-chunk='dashboard-05-chunk-1'>
+                  <CardHeader className='pb-2'>
+                    <CardDescription className='flex gap-2'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        x='0px'
+                        y='0px'
+                        width='28'
+                        height='28'
+                        viewBox='0 0 48 48'>
+                        <path
+                          fill='#FFC107'
+                          d='M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z'></path>
+                        <path
+                          fill='#FF3D00'
+                          d='M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z'></path>
+                        <path
+                          fill='#4CAF50'
+                          d='M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z'></path>
+                        <path
+                          fill='#1976D2'
+                          d='M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z'></path>
+                      </svg>
+                    </CardDescription>
+                    <CardTitle className='text-2xl flex'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='#000000'
+                        stroke='currentColor'
+                        stroke-width='2'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        className='lucide lucide-star'>
+                        <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2' />
+                      </svg>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='#000000'
+                        stroke='currentColor'
+                        stroke-width='2'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        className='lucide lucide-star'>
+                        <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2' />
+                      </svg>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='#000000'
+                        stroke='currentColor'
+                        stroke-width='2'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        className='lucide lucide-star'>
+                        <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2' />
+                      </svg>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='#000000'
+                        stroke='currentColor'
+                        stroke-width='2'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        className='lucide lucide-star'>
+                        <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2' />
+                      </svg>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='#000000'
+                        stroke='currentColor'
+                        stroke-width='2'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        className='lucide lucide-star'>
+                        <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2' />
+                      </svg>
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className='text-xs text-muted-foreground'>Based on 100 reviews</div>
+                  </CardContent>
+                </Card>
+              </a>
             </div>
             <Card x-chunk='dashboard-05-chunk-3'>
               <CardHeader className='px-7'>
