56e68097553fe0f3808d8c151575c28f77847fac
Grace Yong <grace.yong.xue.min@gmail.com>
2024-09-30 22:54:18 +0800

    FE of reward banner

--- app/(protected)/customer/page.tsx
+++ app/(protected)/customer/page.tsx
@@ -1,11 +1,10 @@
 import ShareReferralTile from "./(home)/_components/ShareReferralTile";
+import RewardBanner from "../_components/home/RewardBanner";
 
 export default function CustomerHome() {
   return (
-    <div>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <ShareReferralTile />
+        <RewardBanner />
       </div>
-    </div>
-  );
 }
