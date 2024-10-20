71b1061b8da92a4c506cb3063c3b7623369b2c8c
Grace Yong <grace.yong.xue.min@gmail.com>
2024-10-20 16:32:56 +0800

    combine top nav bar with clerk login

--- app/(protected)/customer/layout.tsx
+++ app/(protected)/customer/layout.tsx
@@ -46,7 +46,7 @@ export default function PublicLayout({
     return (
       <div className="flex min-h-screen w-full flex-col bg-muted/40">
         {/* Top Nav Bar */}
-        <NavigationMenu className="bg-primary w-full h-16 flex items-center justify-between px-8">
+        {/* <NavigationMenu className="bg-primary w-full h-16 flex items-center justify-between px-8">
           <Link href="/customer">
             <Image
               src="/images/logo.jpg"
@@ -126,7 +126,7 @@ export default function PublicLayout({
               </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
-        </NavigationMenu>
+        </NavigationMenu> */}
 
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
---
+++ app/_components/Header.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import CustomerUserButton from "../(protected)/_components/account/CustomerUserButton";
+
+const NAVIGATION_LABELS = {
+  SERVICES: "Services",
+  JOBS: "Jobs",
+  INVOICES: "Invoices",
+  QUOTATIONS: "Quotations",
+  REWARDS: "Rewards",
+};
+
+export default function Header() {
+  const pathname = usePathname();
+
+  return (
+    <header>
+      {/* Top Nav Bar */}
+      <NavigationMenu className="bg-primary w-full h-16 flex items-center justify-between px-8">
+        <Link href="/customer">
+          <Image
+            src="/images/logo.jpg"
+            alt="Repair.sg Logo"
+            width={90}
+            height={90}
+          />
+        </Link>
+
+        <NavigationMenuList>
+          <SignedIn>
+            {Object.entries(NAVIGATION_LABELS).map(([key, label]) => (
+              <NavigationMenuItem key={key}>
+                <Link
+                  href={`/customer/${key.toLowerCase()}`}
+                  legacyBehavior
+                  passHref
+                >
+                  <NavigationMenuLink
+                    className={`${navigationMenuTriggerStyle()} ${
+                      pathname === `/customer/${key.toLowerCase()}`
+                        ? "bg-accent text-accent-foreground"
+                        : "bg-primary"
+                    }`}
+                  >
+                    {label}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </SignedIn>
+          {/* Sign In/Out Button */}
+          <div className="flex justify-between p-4">
+            <SignedIn>
+              <CustomerUserButton />
+            </SignedIn>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+          </div>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+}
