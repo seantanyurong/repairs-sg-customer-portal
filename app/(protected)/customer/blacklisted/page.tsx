c3cf816daf9d85e36c89f849f2f2e036f1e376d7
Sean Tan <seantanyurong@gmail.com>
2024-11-08 12:53:02 +0800

    feature: add blacklist

---
+++ app/(protected)/customer/blacklisted/page.tsx
@@ -0,0 +1,7 @@
+export default function BlacklistedPage() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1>You have been blacklisted. Please contact us at support@repairs.sg if you would like to make an appeal.</h1>
+    </div>
+  );
+}
